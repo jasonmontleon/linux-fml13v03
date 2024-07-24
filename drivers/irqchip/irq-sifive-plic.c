0eb271a4f9ee0e43c8541e562e0799b64643148b
huangyifeng <huangyifeng@eswincomputing.com>
2024-07-24 15:10:18 +0800

    fix(irqchip/sifive-plic): enable interrupt if needed before EOI
    Changelogs:
    
    there is a small window after a hart
    claiming an interrupt and before irq_desc->lock getting acquired,
    interrupt can be disabled during this window. Thus, EOI can be invoked
    while the interrupt is disabled, effectively nullify this EOI.
    Make sure that interrupt is really enabled before EOI
    (Merge from linux 6.10)
    
    Signed-off-by: huangyifeng <huangyifeng@eswincomputing.com>

--- drivers/irqchip/irq-sifive-plic.c
+++ drivers/irqchip/irq-sifive-plic.c
@@ -136,6 +136,10 @@ static void plic_irq_mask(struct irq_data *d)
 
 static void plic_irq_enable(struct irq_data *d)
 {
+#ifdef CONFIG_PM_SLEEP
+	struct irq_desc *desc = irq_data_to_desc(d);
+	/* Avoid reporting errors when removing irq. */
+#endif
 	plic_irq_toggle(irq_data_get_effective_affinity_mask(d), d, 1);
 	plic_irq_unmask(d);
 }
@@ -170,6 +174,7 @@ static int plic_set_affinity(struct irq_data *d,
 	unsigned int cpu;
 	struct cpumask amask;
 	struct plic_priv *priv = irq_data_get_irq_chip_data(d);
+	struct irq_desc *desc = irq_data_to_desc(d);
 
 	cpumask_and(&amask, &priv->lmask, mask_val);
 
@@ -178,8 +183,9 @@ static int plic_set_affinity(struct irq_data *d,
 	else
 		cpu = cpumask_any_and(&amask, cpu_online_mask);
 
-	if (cpu >= nr_cpu_ids)
+	if (cpu >= nr_cpu_ids) {
 		return -EINVAL;
+	}
 
 	/* Invalidate the original routing entry */
 	plic_irq_toggle(irq_data_get_effective_affinity_mask(d), d, 0);
